147a8065bfa7217f8ca8a23b3e0555b685442e12
Motolani Akinyemi <motolaniakinyemi@gmail.com>
2022-03-16 15:12:20 +0100

    assigning event handlers

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ loadEventListeners();
 function loadEventListeners(){
     //Add task event
     form.addEventListener('submit', addTask);
+    //Remove task event
+    taskList.addEventListener('click', removeTask);
+    //Clear Task Event
+    clearBtn.addEventListener('click', clearTasks);
 }
 
 //Add Task
@@ -48,4 +52,19 @@ function addTask(e) {
 taskInput.value = '';
 
     e.preventDefault();
+}
+
+//Remove Task
+function removeTask(e) {
+    if(e.target.parentElement.classList.contains('delete-item')) {
+        if(confirm('Are you sure?')) {   
+        e.target.parentElement.parentElement.remove();
+        }
+    }
+}
+
+//Clear Tasks
+function clearTasks(e) {
+    while(taskList.firstChild)
+    taskList.removeChild(taskList.firstChild);
 }
