dc237cd492202364fbcdd9038f39ebe8782020b7
Motolani Akinyemi <motolaniakinyemi@gmail.com>
2022-03-17 23:45:30 +0100

    local storage

--- app.js
+++ app.js
@@ -49,11 +49,30 @@ function addTask(e) {
 
       //Append the li to the ul
       taskList.appendChild(li);
+
+      //Store in local storage
+      storeTaskInLocalStorage();
+      
+
      
 //Clear input
 taskInput.value = '';
 
     e.preventDefault();
+}
+
+//Store task
+function storeTaskInLocalStorage(task){
+    let tasks;
+    if (localStorage.getItem('tasks') === null ) {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.push(task);
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 //Remove Task
